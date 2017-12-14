4ed37279780bcbe30d6907323a282330244375b9
Joseph Strandmo <josephstrandmo@gmail.com>
2017-12-14 14:31:59 -0600

    Basic content finshed

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -7,10 +7,30 @@
  *      - i.e. the second line in the new file should be the first line of `filePaths[1]`
  *    4. Writes the new file to the file located at `writePath`
  */
-
+var fs = require('fs');
+var request = require('request');
+var Promise = require('bluebird');
 
 var combineFirstLineOfManyFiles = function(filePaths, writePath) {
- // TODO
+  Promise.all(filePaths)
+  .then(function(filePaths) {
+  	return new Promise(function(resolve, reject) { // put lines 18 - 30 in here
+  	
+  	})
+  	var newFile = []
+  	filePaths.forEach(function(filePath) {
+	  fs.readFile(filePath, 'utf8', function(err, text) {
+	    if (err) {
+		  reject(err);
+	    } else {
+	      var firstLine = text.split('\n')[0];
+  	      console.log('FIRSTLINE: ', firstLine)
+	      newFile.push(firstLine);
+	    }
+	  })
+  	});
+  	console.log('newFile: ', newFile); // printing before code above runs
+  })
 };
 
 // Export these functions so we can unit test them
--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -9,12 +9,51 @@
  */
 
 var fs = require('fs');
+var request = require('request');
 var Promise = require('bluebird');
 
-
-
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+	fs.readFile(readFilePath, 'utf8', function(err, text) {
+	  if (err) {
+		reject(err);
+	  } else {
+	    var username = text.split('\n')[0];
+		resolve(username);
+	  }
+	})
+  })
+  .then(function(username) {
+  	return new Promise(function(resolve, reject) {
+	  var options = {
+	    url: 'https://api.github.com/users/' + username,
+	    headers: { 'User-Agent': 'request' },
+	    json: true  // will JSON.parse(body) for us
+	  };
+	  request.get(options, function(err, res, body) {
+	    if (err) {
+	      reject(err);
+	    } else {
+	      resolve(body)
+	    }
+	  });
+  	})
+  })
+  .catch(function(err) {
+  	console.log('error: ', err);
+  })
+  .then(function(body) {
+  	body = JSON.stringify(body);
+  	return new Promise(function(resolve, reject) { // needed to wrap in a promise to prevent test from calling too soon
+  	  fs.writeFile(writeFilePath, body, (err) => {
+  	    if (err) { 
+  	  	  reject(err); 
+  	    } else {
+  	  	  resolve();
+  	    }
+  	  })
+  	});
+  })
 };
 
 // Export these functions so we can test them
